c059c7a8f9b4d127173351d1e0c850021150ac7f
Ana Claudia <ana@anaclaudia.net.br>
2016-08-23 11:25:17 +0200

    Add recipe to install nagios package

--- cookbooks/app/recipes/nagios.rb
+++ cookbooks/app/recipes/nagios.rb
@@ -1,1 +1,10 @@
-itar
+# Download and extract the source code
+tar_extract 'http://prdownloads.sourceforge.net/sourceforge/nagios/nagios-4.2.0.tar.gz' do
+  target_dir '/opt/'
+  creates '/opt/nagios-4.2.0/install-sh.sh'
+end
+
+execute 'rename config dir' do
+  command 'mv /opt/nagios-4.2.0 /opt/nagios'
+  not_if { File.exists?("/opt/nagios/install-sh.sh")}
+end
