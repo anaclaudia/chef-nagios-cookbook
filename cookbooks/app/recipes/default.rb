fdf97f1a818ca781d53baabe8794a0696fd52c78
Ana Claudia <ana@anaclaudia.net.br>
2016-08-24 19:15:18 +0200

    Add new packages (curl and apache) to be installed

--- cookbooks/app/recipes/default.rb
+++ cookbooks/app/recipes/default.rb
@@ -4,6 +4,10 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
+execute 'apt-get update' do
+  command 'sudo apt-get update'
+end
+
 package 'wget' 
 package 'build-essential' 
 package 'apache2'
@@ -11,3 +15,5 @@ package 'php5'
 package 'php5-gd'
 package 'libgd-dev'
 package 'unzip'
+package 'apache2-utils'
+package 'curl'
